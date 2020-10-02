34dfad779ada4972d0e9acce27d75ee016e238cb
yossy0201 <yossy0201msk@gmail.com>
2020-10-02 14:58:29 +0900

    controller

--- src/main/java/com/example/quizapp/QuizAppApplication.java
+++ src/main/java/com/example/quizapp/QuizAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QuizAppApplication {
-
+	　
 	public static void main(String[] args) {
 		SpringApplication.run(QuizAppApplication.class, args);
 	}
