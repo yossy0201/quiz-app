41159fb3a1cf842683ffbba587df6e0e4f37874f
yossy0201 <yossy0201msk@gmail.com>
2020-10-02 15:09:34 +0900

    Hello World

--- src/main/java/com/example/quizapp/QuizAppApplication.java
+++ src/main/java/com/example/quizapp/QuizAppApplication.java
@@ -2,10 +2,11 @@ package com.example.quizapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class QuizAppApplication {
-	　
+	@GetMapping("/show")
 	public static void main(String[] args) {
 		SpringApplication.run(QuizAppApplication.class, args);
 	}
