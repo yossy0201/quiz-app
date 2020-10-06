349ad1b6a733f8a466096c63207ade97bf57f128
yossy0201 <yossy0201msk@gmail.com>
2020-10-07 00:08:11 +0900

    second-argument

--- src/main/java/com/example/quizapp/QuizFileDao.java
+++ src/main/java/com/example/quizapp/QuizFileDao.java
@@ -1,8 +1,10 @@
 package com.example.quizapp;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 //Dao・・・Data access object
@@ -10,9 +12,15 @@ public class QuizFileDao {
 
     private  static final String File_PATH = "quizzes.txt";
 
-    public void write(List<Quiz> quizzes) {
+    public void write(List<Quiz> quizzes) throws IOException {
+    List<String> lines = new ArrayList<>();
+    for (Quiz quiz: quizzes) {
+        lines.add(quiz.toString());
+    }
 
-        Path path = Paths.get(File_PATH)
-        Files.write(path, );
+        Path path = Paths.get(File_PATH);
+        // 第一引数path・・・書き込み先のファイルパス(場所)
+        // 第二引数line・・・書き込むデータ List<String>
+        Files.write(path, lines);
     }
 }
