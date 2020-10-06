94fde2f155cca97d355955a5254f85d89c5497c0
yossy0201 <yossy0201msk@gmail.com>
2020-10-06 23:55:23 +0900

    file-path

--- src/main/java/com/example/quizapp/QuizFileDao.java
+++ src/main/java/com/example/quizapp/QuizFileDao.java
@@ -1,9 +1,12 @@
 package com.example.quizapp;
 
 import java.nio.file.Files;
+import java.util.List;
 
 //Dao・・・Data access object
 public class QuizFileDao {
+
+    private  static final String File_PATH = "quizzes.txt";
 
     public void write(List<Quiz> quizzes) {
 
